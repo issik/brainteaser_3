6dcfcc6b3990060e592cf922cde46a6a00631b62
isaac bustos <isaac.youthboard@futureindesign.org>
2017-12-01 10:29:51 -0700

    i know i'm on to something but not there yet :`

---
+++ magic_number.rb
@@ -0,0 +1,37 @@
+require 'to_words'
+
+puts "Pleease input a number type"
+ gets.strip
+arr == arr.to_words
+
+do(arr).to_word = arr
+until arr = 3
+end
+end
+puts (arr) "is the magic number!!"
+
+
+#number typer out, turned into array
+#number is converted into an array
+#array goes into a do loop
+#loop is ran until four is reached
+#puts magic number if four
+#
+
+#100 = 11
+#11 = 6
+#5=3
+
+#4 is the magic number
+
+#12 = 6
+# 6 =
+#3 =5
+# 4= magic number
+
+ #0 = 4
+
+# 10 = 3 = 5 = 4
+
+ #7=5=4
+# 17=9=4
